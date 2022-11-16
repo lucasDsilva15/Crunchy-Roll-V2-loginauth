b97288668c73bc498a63ff2b8baf93b58e206ba1
Lucas Da Silva <lucasilva15@live.com>
2022-11-16 18:09:52 -0500

    trying to push

--- src/pages/Profile.js
+++
@@ -1,9 +0,0 @@
-export default function Profile ({username, email}) {
-    return (
-        <div>
-            <h1>Profile</h1>
-            <p> Username: {username}</p>
-            <p> E-Mail: {email}</p>
-        </div>
-    )
-}
